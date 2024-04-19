b95d93c1ff441735a7ffc1743fe80e5e8f135930
LAPTOP-TQF4G7PI\Nguyễn Thắng <thangncph20964@fpt.edu.vn>
2024-04-19 21:56:41 +0700

    doing candidate

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { Checkbox } from '@nextui-org/react'
+import { Checkbox } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
 import React from 'react'
+import { FaBeer } from 'react-icons/fa';
 
 const LoginPage = () => {
     const router = useRouter()
@@ -25,12 +26,12 @@ const LoginPage = () => {
                 <div className="relative flex flex-col sm:w-[30rem] rounded-lg border-gray-400 bg-white shadow-lg px-4">
                     <div className="flex-auto p-6">
                         <div className="mb-10 flex flex-shrink-0 flex-grow-0 items-center justify-center overflow-hidden">
-                            <a href="#" className="flex cursor-pointer items-center gap-2 text-indigo-500 no-underline hover:text-indigo-500">
-                                <span className="flex-shrink-0 text-3xl font-black lowercase tracking-tight opacity-100">Futurism.</span>
+                            <a href="#" className="flex cursor-pointer items-center gap-2 bg-gradient-to-r from-sky-400 to-blue-500  bg-clip-text text-transparent no-underline">
+                                <span className="flex-shrink-0 text-3xl font-black  tracking-tight opacity-100">TEAM 01 VÔ ĐỊCH</span>
                             </a>
                         </div>
-                        <h4 className="mb-2 font-medium text-gray-700 xl:text-xl">Welcome to futurism!</h4>
-                        <p className="mb-6 text-gray-500">Please sign-in to access your account</p>
+                        <h4 className="mb-2 font-medium text-center  text-gray-700 xl:text-xl">Welcome to Channel của tụi t:))</h4>
+                        <p className="mb-6 text-center text-gray-500">Please sign-in to access your account</p>
 
                         <form id="" className="mb-4" action="#" method="POST">
                             <div className="mb-4">
@@ -42,7 +43,7 @@ const LoginPage = () => {
                                 <div className="flex justify-between">
                                     <label className="mb-2 inline-block text-xs font-medium uppercase text-gray-700"
                                         htmlFor="password">Password</label>
-                                    <a href="auth-forgot-password-basic.html" className="cursor-pointer text-indigo-500 no-underline hover:text-indigo-500">
+                                    <a href="auth-forgot-password-basic.html" className="cursor-pointer text-black no-underline hover:text-blue-500">
                                         <small className=" ">Forgot Password?</small>
                                     </a>
                                 </div>
@@ -52,24 +53,29 @@ const LoginPage = () => {
                             </div>
                             <div className="mb-4">
                                 <div className="block">
-                                    <input className="mt-1 mr-2 h-5 w-5 appearance-none rounded border border-gray-300 bg-contain bg-no-repeat align-top text-black shadow checked:bg-indigo-500 focus:border-indigo-500 focus:shadow" type="checkbox" id="remember-me" checked />
-                                    <label className="inline-block" htmlFor="remember-me"> Remember Me </label>
+                                    <Checkbox color='primary' defaultSelected>Remember me?</Checkbox>
                                 </div>
                             </div>
                             <div className="mb-4">
-                                <div onClick={handleSignIn} className="grid w-full cursor-pointer select-none rounded-md border border-indigo-500 bg-indigo-500 py-2 px-5 text-center align-middle text-sm text-white shadow hover:border-indigo-600 hover:bg-indigo-600 hover:text-white focus:border-indigo-600
+                                <div onClick={handleSignIn} className="grid w-full cursor-pointer select-none rounded-md border 
+                                    bg-gradient-to-r from-sky-400 to-blue-500 bg-indigo-500 py-2 px-5 
+                                    text-center align-middle text-sm text-white shadow font-bold
+                                    hover:border-indigo-600 hover:bg-indigo-600 hover:text-white focus:border-indigo-600
                                  focus:bg-indigo-600 focus:text-white focus:shadow-none" >Sign in</div>
                             </div>
                         </form>
 
                         <p className="mb-4 text-center">
+                        
                             New on futurism?
-                            <a href="#" className="cursor-pointer text-indigo-500 no-underline hover:text-indigo-500"> Create an account </a>
+                            <a href="#" className="cursor-pointer text-black no-underline hover:text-blue-500"> Create an account </a>
                         </p>
                     </div>
                 </div>
             </div>
         </div>
+
+
 
     )
 }
